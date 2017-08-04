241f8f815b0b3bfe2eaf0e8182d85919d1de57b2
Lingcao Huang <huanglingcao@gmail.com>
2017-08-04 23:16:00 +0800

    set use gpu:1

--- mnist_tutorial.py
+++ mnist_tutorial.py
@@ -15,7 +15,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 
-with tf.device('/cpu:0'):
+with tf.device('/gpu:1'):
     x = tf.placeholder(tf.float32,shape=[None,784])
     y_ = tf.placeholder(tf.float32,shape=[None,10])
 
