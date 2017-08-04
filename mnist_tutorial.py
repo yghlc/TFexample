0f64804a63a554982fbdf89782d1efd2cdbe4c80
Lingcao Huang <huanglingcao@gmail.com>
2017-08-04 16:11:56 +0800

    output test accuracy in every 100 iteration

--- mnist_tutorial.py
+++ mnist_tutorial.py
@@ -123,6 +123,10 @@ with tf.Session() as sess:
         if i%100 == 0:
             train_accuracy = accuracy.eval(feed_dict={x:batch[0],y_:batch[1],keep_prob:1.0})
             print('step %d, training accuracy %g '%(i, train_accuracy))
+
+            test_accuracy = accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
+            print('test accuracy %g' % test_accuracy)
+
         train_step.run(feed_dict={x:batch[0],y_:batch[1],keep_prob:0.5})
 
     test_accuracy = accuracy.eval(feed_dict={x:mnist.test.images,y_:mnist.test.labels, keep_prob:1.0})
