dd1a35e0c37290ff3bdd75d3b0368d867f76664c
Lingcao Huang <huanglingcao@gmail.com>
2017-08-04 21:35:10 +0800

    set gpu

--- mnist_tutorial.py
+++ mnist_tutorial.py
@@ -116,21 +116,24 @@ train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv,1),tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
 
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    for i in range(20000):
-        batch = mnist.train.next_batch(50)
-        if i%100 == 0:
-            train_accuracy = accuracy.eval(feed_dict={x:batch[0],y_:batch[1],keep_prob:1.0})
-            print('step %d, training accuracy %g '%(i, train_accuracy))
+# set gpu.
+with tf.device("/gpu:0"):
 
-            # test_accuracy = accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
-            # print('test accuracy %g' % test_accuracy)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for i in range(20000):
+            batch = mnist.train.next_batch(50)
+            if i%100 == 0:
+                train_accuracy = accuracy.eval(feed_dict={x:batch[0],y_:batch[1],keep_prob:1.0})
+                print('step %d, training accuracy %g '%(i, train_accuracy))
 
-        train_step.run(feed_dict={x:batch[0],y_:batch[1],keep_prob:0.5})
+                # test_accuracy = accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
+                # print('test accuracy %g' % test_accuracy)
 
-    test_accuracy = accuracy.eval(feed_dict={x:mnist.test.images,y_:mnist.test.labels, keep_prob:1.0})
-    print('test accuracy %g'%test_accuracy )
+            train_step.run(feed_dict={x:batch[0],y_:batch[1],keep_prob:0.5})
+
+        test_accuracy = accuracy.eval(feed_dict={x:mnist.test.images,y_:mnist.test.labels, keep_prob:1.0})
+        print('test accuracy %g'%test_accuracy )
 
 
 
